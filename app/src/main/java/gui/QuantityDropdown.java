aeae91ca3e97ff5032dbc997e1d5df16a2004432
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-04-17 22:40:33 +0700

    add: Util.setCurrentQuantity

--- app/src/main/java/gui/QuantityDropdown.java
+++ app/src/main/java/gui/QuantityDropdown.java
@@ -8,16 +8,18 @@ import logic.Util;
 
 public class QuantityDropdown extends ComboBox<String>{
     public QuantityDropdown(){
-        String[] quantities = Util.getQuantities();
+        String[] quantities = Util.getQuantityNames();
         Arrays.sort(quantities);
         for(String quantityName : quantities){
             getItems().add(quantityName);
         }
         
         setValue(quantities[0]);
+        Util.setCurrentQuantity(quantities[0]);
 
         setOnAction(e -> {
             String quantity = getValue();
+            Util.setCurrentQuantity(quantity);
         });
     }
 }
--- app/src/main/java/logic/Util.java
+++ app/src/main/java/logic/Util.java
@@ -2,26 +2,29 @@ package logic;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
-import javafx.scene.control.ComboBox;
 import quantity.Length;
 import quantity.Quantity;
 
 public class Util {
     private static Map<String, Quantity> quantityMap;
     private static final Length length = new Length();
+    private static Quantity currentQuantity;
 
     public static void initialize(){
         quantityMap = new HashMap<String, Quantity>();
         quantityMap.put("Length", length);
     }
 
-    public static String[] getQuantities(){
+    public static String[] getQuantityNames(){
         return quantityMap.keySet().toArray(String[]::new);
     }
 
-    public ComboBox<String> getLengthDropDown(){
-        return length.getUnit();
+    public static Quantity getQuantityObjects(String name){
+        return quantityMap.get(name);
+    }
+
+    public static void setCurrentQuantity(String name){
+        currentQuantity = getQuantityObjects(name);
     }
 }
