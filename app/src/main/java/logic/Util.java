2ef8c8a323585a42fb49f5f535d9ca9f3267cccc
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-04-17 21:58:20 +0700

    add: Util.initialize()

--- app/src/main/java/logic/Util.java
+++ app/src/main/java/logic/Util.java
@@ -1,5 +1,16 @@
 package logic;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import quantity.Length;
+import quantity.Quantity;
+
 public class Util {
-    
+    private static Map<String, Quantity> quantityMap;
+
+    public static void initialize(){
+        quantityMap = new HashMap<String, Quantity>();
+        quantityMap.put("Length", new Length());
+    }
 }
