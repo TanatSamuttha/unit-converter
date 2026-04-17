b52f3cb419b852e1f7d95566fc8f3f9b9f9ac727
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-04-17 22:30:15 +0700

    create: Form

---
+++ app/src/main/java/gui/Form.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class Form extends Pane {
+    public static ComboBox<String> unit;
+    public static TextField field;
+}
